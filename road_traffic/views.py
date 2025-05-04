00202c039b5e359ed237239801663f286c4d93bf
W4cher <wachergarcia@gmail.com>
2025-05-04 17:50:53 +0100

    Adicione os novos endpoints para passagens e permicoes

--- road_traffic/views.py
+++ road_traffic/views.py
@@ -1,13 +1,15 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from django_filters.rest_framework import DjangoFilterBackend
-from .models import RoadSegment, Reading
-from .serializers import RoadSegmentSerializer, ReadingSerializer
-from .permissions import IsAdminOrReadOnly
+from .models import RoadSegment, Reading, VehiclePassage
+from .serializers import RoadSegmentSerializer, ReadingSerializer, VehiclePassageSerializer
+from .permissions import IsAdminOrReadOnly, HasSensorAPIKey
 from django.db.models import Max, Subquery, OuterRef
+from rest_framework.permissions import IsAdminUser
+from django.utils import timezone
+from datetime import timedelta
 import logging
 
 logger = logging.getLogger(__name__)
-
 
 class RoadSegmentViewSet(viewsets.ModelViewSet):
     queryset = RoadSegment.objects.all()
@@ -30,7 +32,6 @@ class RoadSegmentViewSet(viewsets.ModelViewSet):
             queryset = queryset.annotate(
                 last_intensity=Subquery(latest_reading)
             ).filter(last_intensity=last_reading_intensity)
-
         return queryset
 
 class ReadingViewSet(viewsets.ModelViewSet):
@@ -42,5 +43,33 @@ class ReadingViewSet(viewsets.ModelViewSet):
         logger.info("Accessing ReadingViewSet")
         return super().get_queryset()
 
+class VehiclePassageViewSet(viewsets.ModelViewSet):
+    queryset = VehiclePassage.objects.all()
+    serializer_class = VehiclePassageSerializer
+    permission_classes = [HasSensorAPIKey]
+    http_method_names = ['post']
 
+    def get_queryset(self):
+        logger.info("Accessing VehiclePassageViewSet")
+        return super().get_queryset()
+
+class VehiclePassageByPlateList(generics.ListAPIView):
+    serializer_class = VehiclePassageSerializer
+    permission_classes = [IsAdminUser]
+
+    def get_queryset(self):
+        logger.info("Accessing VehiclePassageByPlateList")
+        license_plate = self.request.query_params.get('license_plate')
+        if not license_plate:
+            logger.warning("Missing license_plate parameter")
+            return VehiclePassage.objects.none()
+        
+        # Filtrar passagens das últimas 24 horas
+        time_threshold = timezone.now() - timedelta(hours=24)
+        queryset = VehiclePassage.objects.filter(
+            car__license_plate=license_plate,
+            timestamp__gte=time_threshold
+        ).select_related('road_segment', 'car', 'sensor')
+        return queryset
+    
     
